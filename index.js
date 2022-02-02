d0660d686d91e2a5b04e78d0e77094fba65405bd
suntoes <mar26xd@gmail.com>
2022-02-02 23:30:36 +0800

    codeManualCleanUp();

--- index.js
+++ index.js
@@ -29,83 +29,90 @@ let speed = 600;
 
 let defPass = true;
 
+const boardDiv = document.getElementById('board-div');
+const gameButton = document.getElementById('game-button');
+const controlToggleButton = document.getElementById('control-toggle');
+const controlsButton = document.querySelectorAll('.controls-button');
+const controlDiv = document.getElementById('#controls-div');
+
 // initial draw call
 draw(board);
 
 // early DOM configuration
-document.querySelector('#game-button').onclick = gameOn;
-document.querySelector('#control-toggle').onclick = controlsToggle;
-document.querySelectorAll('.controls-button').forEach(btn => btn.onclick = function(){inputKey(btn.id)});
+gameButton.onclick = gameOn;
+controlToggleButton.onclick = controlsToggle;
+controlsButton.forEach(btn => btn.onclick = function(){inputKey(btn.id)});
 
 // control essentials
 document.addEventListener('keydown', (e) => {
     inputKey(e.key.toLowerCase());
 })
+
 function controlsToggle() {
-    if (document.querySelector('#control-toggle').innerHTML === "show control buttons?") {
-        document.querySelector('#controls-div').style.display = "flex";
-        document.querySelector('#control-toggle').innerHTML = "hide control buttons?"
+    if (controlToggleButton.innerHTML === "show control buttons?") {
+        controlDiv.style.display = "flex";
+        controlToggleButton.innerHTML = "hide control buttons?"
     } else {
-        document.querySelector('#controls-div').style.display = "none";
-        document.querySelector('#control-toggle').innerHTML = "show control buttons?"
+        controlDiv.style.display = "none";
+        controlToggleButton.innerHTML = "show control buttons?"
     }
 }
+
 function inputKey(key) {
-    if (document.querySelector('#game-button').innerHTML !== 'click me to play!' && document.querySelector('#game-button').innerHTML !== 'aw! wanna try again?') {
+    if (gameButton.innerHTML !== 'click me to play!' && gameButton.innerHTML !== 'aw! wanna try again?') {
         switch(key) {
             case 'arrowup':
             case 'w':
                 if (snakeIndex[0]-8 === snakeIndex[1]) {
                     break;
-                    } else {
-                        moveUp = true;
-                        moveRight = false;
-                        moveLeft = false;
-                        moveDown = false;
-                }
+                } else {
+                    moveUp = true;
+                    moveRight = false;
+                    moveLeft = false;
+                    moveDown = false;
+                    }
                 break;
             case 'arrowleft':
             case 'a':
                 if (snakeIndex[0]-1 === snakeIndex[1]) {
                     break;
-                    } else {
-                        moveLeft = true;
-                        moveUp = false;
-                        moveRight = false;
-                        moveDown = false;
+                } else {
+                    moveLeft = true;
+                    moveUp = false;
+                    moveRight = false;
+                    moveDown = false;
                 }
                 break;
             case 'arrowright':
             case 'd':
                 if (snakeIndex[0]+1 === snakeIndex[1]) {
-                    break;
-                    } else {
-                        moveRight = true;
-                        moveUp = false;
-                        moveLeft = false;
-                        moveDown = false;
+                break;
+                } else {
+                    moveRight = true;
+                    moveUp = false;
+                    moveLeft = false;
+                    moveDown = false;
                 }
                 break;
             case 'arrowdown':
             case 's':
                 if (snakeIndex[0]+8 === snakeIndex[1]) {
-                    break;
-                    } else {
+                break;
+                } else {
                         moveDown = true;
                         moveUp = false;
                         moveRight = false;
                         moveLeft = false;
-                }
+                    }
                 break;
         };
-    }
-    
-}
+    }         
+};
 
 // sets new speed
 function newSpeed() {
     speed = 600 -(( 1/( board.length/snakeIndex.length) )*200);
-}
+};
 
 // sets new list of snake-free index for next apple
 function refreshAppleFree() {
@@ -124,41 +131,86 @@ function newAppleIndex() {
     return rndm;
 }
 
+// resets the #board-div with a new fresh updated divs ;)
+function draw(board) {
+    if (boardDiv.innerHTML === '') {
+        board.forEach(( e, i) => {
+
+            const cell = document.createElement('div')
+            boardDiv.appendChild(cell);
+
+            cell.id = i
+            cell.classList.add('cells');
+            cell.style.backgroundColor = '#212121';
+
+            detectSnakeOrApple(cell, i);
+        });
+    }   else {
+            board.forEach(( e, i) => {
+                const cell = document.getElementById(i);
+                cell.style.backgroundColor = '#212121';
+
+                if(cell.innerHTML === ':' && i !== snakeIndex[0]) {
+                    cell.innerHTML = '';
+                    cell.classList.remove('cell-head');
+                    cell.style.transform = '';
+                }
+
+                detectSnakeOrApple(cell, i);
+            });
+        }
+}
+
+function detectSnakeOrApple(cell, i) {
+    if(snakeIndex.some(x => x === i)) {
+        if(i === snakeIndex[0]) {
+            cell.classList.add('cell-head');
+            cell.innerHTML = ':';
+
+            let numDeg = moveUp ? 90 : moveRight ? 180 : moveDown ? 270 : 0
+            cell.style.transform = `rotate(${numDeg}deg)`
+        };    
+        cell.style.backgroundColor = '#4df163';
+    }   else if(i === appleIndex) {
+            cell.style.backgroundColor = '#ff5959';
+        };
+}
+
 // resets necessary game elements
 function reset() {
-
+    
     snakeIndex = [75,83];
-
+    
     appleIndex = 20;
     listOfAppleFree = []
-
+    
     moveLeft = false;
     moveRight = false;
     moveUp = true;
     moveDown = false;
     
     speed = 600;
-
-    document.querySelector('#board-div').onclick = () => {defPass = true; gameOn()};
-    document.querySelector('#game-button').onclick = () => {defPass = true; gameOn()};
-    document.querySelector('#game-button').innerHTML = 'click me to play!';
-    document.querySelector('#board-div').style.filter = '';
-
+    
+    boardDiv.onclick = () => {defPass = true; gameOn()};
+    gameButton.onclick = () => {defPass = true; gameOn()};
+    gameButton.innerHTML = 'click me to play!';
+    boardDiv.style.filter = '';
+    
 };
 
 // starts the game with setInterval() and renderBoard()
 function gameOn() {
-    document.querySelector('#board-div').style.filter = ''
+    boardDiv.style.filter = ''
     // recalls renderBoard() for every speed in milliseconds
     let interval = setInterval(() => renderBoard(), speed);
-
+    
     // instruction before first apple gets eaten
-    document.querySelector('#game-button').innerHTML = '(w, a, s, d) are the controls!';
-
+    gameButton.innerHTML = '(w, a, s, d) are the controls!';
+    
     // disable the unnecessary button for gameplay
-    document.querySelector('#board-div').onclick = '';
-    document.querySelector('#game-button').onclick = '';
-
+    boardDiv.onclick = '';
+    gameButton.onclick = '';
+    
     // use of localStorage to clean interval later on other function
     localStorage.clear();
     localStorage.setItem('game-interval', interval);
@@ -182,7 +234,7 @@ function renderBoard() {
         gameOn();
 
         // gameboard update
-        document.querySelector('#game-button').innerHTML = 
+        gameButton.innerHTML = 
             `<div class="apple-count">${94-(board.length - snakeIndex.length)}/94</div> &nbsp; apples, &nbsp;<div class="speed-count">${(600/speed).toFixed(2)}x</div> &nbsp; speed`;
     } else {
         // else if snake dont eats apple
@@ -203,7 +255,6 @@ function gameOff() {
     clearInterval( localStorage.getItem('game-interval') );
 }
 
-
 function snakeMove(pos) {
     // basic math to move snake in array w/ restrictions
     if (moveUp) {
@@ -221,8 +272,8 @@ function winTest() {
     // basic logic to detect win win
     if (snakeIndex.length >= board.length) {
         gameOff();
-        document.querySelector('#game-button').innerHTML = 'nc! wanna play again?';
-        document.querySelector('#game-button').onclick = gameOn;
+        gameButton.innerHTML = 'nc! wanna play again?';
+        gameButton.onclick = gameOn;
     }
 }
 
@@ -232,8 +283,8 @@ function defTest() {
         reset();
         gameOff();
         defPass = false;
-        document.querySelector('#game-button').innerHTML = 'aw! wanna try again?';
-        document.querySelector('#board-div').style.filter = 'brightness(80%)';
+        gameButton.innerHTML = 'aw! wanna try again?';
+        boardDiv.style.filter = 'brightness(80%)';
     };
 
     // some impromptu maths to detect snake move violations
@@ -245,59 +296,4 @@ function defTest() {
         provenLose();
     }
     
-}
-
-// resets the #board-div with a new fresh updated divs ;)
-function draw(board) {
-    if (document.querySelector('#board-div').innerHTML === '') {
-        board.forEach(( e, i) => {
-
-            const cell = document.createElement('div')
-            document.getElementById('board-div').appendChild(cell);
-
-            cell.id = i
-            cell.classList.add('cells');
-            cell.style.backgroundColor = '#212121';
-
-            if(snakeIndex.some(x => x === i)) {
-                if(i === snakeIndex[0]) {
-                    cell.classList.add('cell-head');
-                    cell.innerHTML = ':';
-    
-                    let numDeg = moveUp ? 90 : moveRight ? 180 : moveDown ? 270 : 0
-                    cell.style.transform = `rotate(${numDeg}deg)`
-                };
-                cell.style.backgroundColor = '#4df163';
-            }   else if(i === appleIndex) {
-                    cell.style.backgroundColor = '#ff5959';
-                }
-  
-        });
-    }   else {
-            board.forEach(( e, i) => {
-
-                const cell = document.getElementById(i);
-                cell.style.backgroundColor = '#212121';
-                if(cell.innerHTML === ':' && i !== snakeIndex[0]) {
-                    cell.innerHTML = '';
-                    cell.classList.remove('cell-head');
-                    cell.style.transform = '';
-                }
-
-                if(snakeIndex.some(x => x === i)) {
-                    if(i === snakeIndex[0]) {
-                        cell.classList.add('cell-head');
-                        cell.innerHTML = ':';
-    
-                        let numDeg = moveUp ? 90 : moveRight ? 180 : moveDown ? 270 : 0
-                        cell.style.transform = `rotate(${numDeg}deg)`
-                    };    
-                    cell.classList.add('cells');
-                    cell.style.backgroundColor = '#4df163';
-                }   else if(i === appleIndex) {
-                        cell.style.backgroundColor = '#ff5959';
-                        cell.innerHTML = '';
-                    };
-            });
-        }
 }
